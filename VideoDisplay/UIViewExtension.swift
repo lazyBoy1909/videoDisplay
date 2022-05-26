d60a2927bff416fdfa628ea05500f092fbd073b4
đào sơn <daoson@MacBook-Air-cua-ao.local>
2022-05-26 16:43:52 +0700

    update change

--- VideoDisplay/UIViewExtension.swift
+++ VideoDisplay/UIViewExtension.swift
@@ -6,17 +6,3 @@
 //
 
 import Foundation
-import UIKit
-extension UIView{
-    func animShow(){
-        UIView.animate(withDuration: 0.25, animations: {
-            self.isHidden = false
-            self.transform = CGAffineTransform(translationX: 0, y: 0)
-        })
-    }
-    func animHide(){
-        UIView.animate(withDuration: 0.25, animations: {
-            self.transform = CGAffineTransform(translationX: 0, y: self.bounds.height + 200)
-        })
-    }
-}
