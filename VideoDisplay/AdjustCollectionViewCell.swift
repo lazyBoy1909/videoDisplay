dfb2cda5bae0cad1660b20566856c7fc11b405a6
đào sơn <daoson@MacBook-Air-cua-ao.local>
2022-05-27 17:13:45 +0700

    adjust circular progress bar accord to sliding ruler

--- VideoDisplay/AdjustCollectionViewCell.swift
+++ VideoDisplay/AdjustCollectionViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 class AdjustCollectionViewCell: UICollectionViewCell {
-<<<<<<< HEAD
+
 
     @IBOutlet private weak var itemImageView: UIImageView!
     var circularProgressView: CircularProgressView!
@@ -16,28 +16,14 @@ class AdjustCollectionViewCell: UICollectionViewCell {
     {
         self.circularProgressView = CircularProgressView(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height))
         self.addSubview(circularProgressView)
-=======
-    @IBOutlet private weak var itemContentView: UIView!
-    let shape = CAShapeLayer()
-    var itemImage: [UIImage?] = [UIImage(named: "adj1"), UIImage(named: "adj2"), UIImage(named: "adj3"), UIImage(named: "adj4"), UIImage(named: "adj5"), UIImage(named: "adj6"), UIImage(named: "adj7")]
-    
-    func createCircularProgressBar()
-    {
-        let circlePath = UIBezierPath(arcCenter: self.center, radius: self.bounds.height/3, startAngle: 0, endAngle: .pi*2, clockwise: true)
-        shape.path = circlePath.cgPath
-        shape.lineWidth = 3
-        shape.strokeColor = UIColor.red.cgColor
-        shape.fillColor = UIColor.clear.cgColor
-        shape.strokeEnd = 1
-        self.layer.addSublayer(shape)
->>>>>>> 31dc929 (first commit,create adjust view with animation up and down, create basic circular progress bar)
+
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
         createCircularProgressBar()
     }
-<<<<<<< HEAD
+
 
     func initAdjustCell(itemImageForCell imageCell: UIImage?)
     {
@@ -80,15 +66,38 @@ class AdjustCollectionViewCell: UICollectionViewCell {
         valueLabel.text = "\((Int)(offset/10))"
         valueLabel.textColor = .white
         self.addSubview(valueLabel)
-=======
-    func initAdjustCell(indexPath: IndexPath)
+
+    }
+    
+    func adjustCircularProgressBarItem(currentValue offset: Double)
     {
-        if let image = itemImage[indexPath.row]
+        shape.strokeEnd = offset/1000
+    }
+    
+    func changeToNumberDisplayState(currentValue offset: Double)
+    {
+        self.subviews.forEach
         {
-            let imageView = UIImageView(image: image)
-            imageView.frame = CGRect(x: 14.5, y: 14.5, width: contentView.bounds.width/2-5, height: contentView.bounds.height/2-5)
-            contentView.addSubview(imageView)
+            $0.isHidden = true
         }
->>>>>>> 31dc929 (first commit,create adjust view with animation up and down, create basic circular progress bar)
+        let valueLabel = UILabel()
+        if(offset < 100)
+        {
+            valueLabel.frame = CGRect(x: 20, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
+            valueLabel.font = .systemFont(ofSize: 15)
+        }
+        else if(offset >= 100 && offset < 1000)
+        {
+            valueLabel.frame = CGRect(x: 16, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
+            valueLabel.font = .systemFont(ofSize: 15)
+        }
+        else
+        {
+            valueLabel.frame = CGRect(x: 13, y: 11, width: self.bounds.width/2-2, height: self.bounds.height/2-2)
+            valueLabel.font = .systemFont(ofSize: 13)
+        }
+        valueLabel.text = "\((Int)(offset/10))"
+        valueLabel.textColor = .white
+        self.addSubview(valueLabel)
     }
 }
