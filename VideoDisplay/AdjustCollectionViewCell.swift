53f687733956d32d63a2c30d074b4af4f1a8f4d2
đào sơn <daoson@MacBook-Air-cua-ao.local>
2022-05-27 17:13:45 +0700

    adjust circular progress bar accord to sliding ruler

--- VideoDisplay/AdjustCollectionViewCell.swift
+++ VideoDisplay/AdjustCollectionViewCell.swift
@@ -13,9 +13,9 @@ class AdjustCollectionViewCell: UICollectionViewCell {
     
     func createCircularProgressBar()
     {
-        let circlePath = UIBezierPath(arcCenter: self.center, radius: self.bounds.height/3, startAngle: 0, endAngle: .pi*2, clockwise: true)
+        let circlePath = UIBezierPath(arcCenter: self.center, radius: self.bounds.height/3, startAngle: .pi * 3/2, endAngle: .pi * -1/2, clockwise: false)
         shape.path = circlePath.cgPath
-        shape.lineWidth = 3
+        shape.lineWidth = 2
         shape.strokeColor = UIColor.red.cgColor
         shape.fillColor = UIColor.clear.cgColor
         shape.strokeEnd = 1
@@ -31,8 +31,40 @@ class AdjustCollectionViewCell: UICollectionViewCell {
         if let image = itemImage[indexPath.row]
         {
             let imageView = UIImageView(image: image)
-            imageView.frame = CGRect(x: 14.5, y: 14.5, width: contentView.bounds.width/2-5, height: contentView.bounds.height/2-5)
-            contentView.addSubview(imageView)
+            imageView.frame = CGRect(x: 14, y: 14, width: self.bounds.width/2-5, height: self.bounds.height/2-5)
+            self.addSubview(imageView)
         }
+    }
+    
+    func adjustCircularProgressBarItem(currentValue offset: Double)
+    {
+        shape.strokeEnd = offset/1000
+    }
+    
+    func changeToNumberDisplayState(currentValue offset: Double)
+    {
+        self.subviews.forEach
+        {
+            $0.isHidden = true
+        }
+        let valueLabel = UILabel()
+        if(offset < 100)
+        {
+            valueLabel.frame = CGRect(x: 20, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
+            valueLabel.font = .systemFont(ofSize: 15)
+        }
+        else if(offset >= 100 && offset < 1000)
+        {
+            valueLabel.frame = CGRect(x: 16, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
+            valueLabel.font = .systemFont(ofSize: 15)
+        }
+        else
+        {
+            valueLabel.frame = CGRect(x: 13, y: 11, width: self.bounds.width/2-2, height: self.bounds.height/2-2)
+            valueLabel.font = .systemFont(ofSize: 13)
+        }
+        valueLabel.text = "\((Int)(offset/10))"
+        valueLabel.textColor = .white
+        self.addSubview(valueLabel)
     }
 }
