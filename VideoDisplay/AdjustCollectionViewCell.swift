0fd06ebdd99b219820f4b6d81564cfae067223b7
lazyBoy1909 <93031924+lazyBoy1909@users.noreply.github.com>
2022-06-03 15:00:04 +0700

    update 2 func changeDisplay, use global variable to store and update

--- VideoDisplay/AdjustCollectionViewCell.swift
+++ VideoDisplay/AdjustCollectionViewCell.swift
@@ -9,7 +9,7 @@ import UIKit
 class AdjustCollectionViewCell: UICollectionViewCell {
     @IBOutlet private weak var itemImageView: UIImageView!
     var circularProgressView: CircularProgressView!
-    var countLabel: UILabel? = nil
+    var countLabel: UILabel = UILabel()
     func createCircularProgressBar()
     {
         self.circularProgressView = CircularProgressView(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: self.bounds.height))
@@ -19,6 +19,7 @@ class AdjustCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         createCircularProgressBar()
+        self.addSubview(countLabel)
     }
     func initAdjustCell(itemImageForCell imageCell: UIImage?)
     {
@@ -38,48 +39,29 @@ class AdjustCollectionViewCell: UICollectionViewCell {
     
     func changeToImageDisplayState()
     {
-        self.subviews.forEach
-        {
-            if($0 is UILabel)
-            {
-                $0.isHidden = true
-            }
-            else
-            {
-                $0.isHidden = false
-            }
-        }
+        itemImageView.isHidden = false
+        countLabel.isHidden = true
+        
     }
     func changeToNumberDisplayState(currentValue offset: Double)
     {
-        var valueLabel = UILabel()
-        for subview in self.subviews {
-            if subview is UILabel {
-                // existed a label
-                subview.isHidden = false
-                valueLabel = subview as! UILabel
-            }
-            else
-            {
-                subview.isHidden = true
-            }
-        }
-        
+        countLabel.isHidden = false
+        itemImageView.isHidden = true
         //add value label to cell
         if(offset < 100)
         {
-            valueLabel.frame = CGRect(x: 20, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
-            valueLabel.font = UIFont(name: "Montserrat-Bold", size: 15)
+            countLabel.frame = CGRect(x: 20, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
+            countLabel.font = UIFont(name: "Montserrat-Bold", size: 15)
         }
         else if(offset >= 100 && offset < 1000)
         {
-            valueLabel.frame = CGRect(x: 16, y: 14, width: self.bounds.width/2-8, height: self.bounds.height/2-8)
-            valueLabel.font = UIFont(name: "Montserrat-Bold", size: 14)
+            countLabel.frame = CGRect(x: 16, y: 14, width: self.bounds.width/2, height: self.bounds.height/2)
+            countLabel.font = UIFont(name: "Montserrat-Bold", size: 14)
         }
         else
         {
-            valueLabel.frame = CGRect(x: 13, y: 11, width: self.bounds.width/2-2, height: self.bounds.height/2-2)
-            valueLabel.font = UIFont(name: "Montserrat-Bold", size: 13)
+            countLabel.frame = CGRect(x: 11, y: 11, width: self.bounds.width/2+3, height: self.bounds.height/2+3)
+            countLabel.font = UIFont(name: "Montserrat-Bold", size: 13)
         }
         let ratioValue: Int
         if(offset > 1000)
@@ -94,18 +76,8 @@ class AdjustCollectionViewCell: UICollectionViewCell {
         {
             ratioValue = (Int)(offset/10)
         }
-        valueLabel.text = "\(ratioValue)"
-        valueLabel.textColor = .white
-        
-        //if existed a label, then no need to add a new subview
-        if(countLabel == nil)
-        {
-            self.addSubview(valueLabel)
-        }
-        else
-        {
-            countLabel = valueLabel
-        }
+        countLabel.text = "\(ratioValue)"
+        countLabel.textColor = .white
         
     }
 }
